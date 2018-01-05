d068ba9c267d56732888e64051bcd6073765befc
Mike Sherman <msherman83@gmail.com>
2018-01-05 14:24:42 -0500

    initial commit with layout

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Monster", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Daddy", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Little Guy", TRUE);
