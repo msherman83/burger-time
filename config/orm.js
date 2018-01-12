c1f82c6d239c6cea30ba3ef1c9aca701eb431c35
Mike Sherman <msherman83@gmail.com>
2018-01-12 18:01:38 -0500

    added css styling and bootstrap

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,6 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
-// REPLACE AS WELL?
-
-
 // Helper function for SQL syntax.
 // Let's say we want to pass 3 values into the mySQL query.
 // In order to write the query, we need 3 question marks.
@@ -44,15 +41,6 @@ function printQuestionMarks(num) {
 
 
 var orm = {
-//     selectAll();
-
-// insertOne();
-
-// updateOne();,
-
-
-
-// REPLACE THIS STUFF!
 
 selectAll: function (tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
